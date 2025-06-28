7dfbc4117bcb5cf5b40316d1fc25553a48e2c711
IcQby <co.steven@gmail.com>
2025-06-28 14:41:36 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,38 +1,13 @@
-const cacheName = 'retro-pet-cache-v1';
-const filesToCache = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/app.js',
-  '/manifest.json',
-  '/retro_pet_pig_icon_192.png',
-  '/retro_pet_pig_icon_512.png'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(cacheName)
-      .then(cache => cache.addAll(filesToCache))
-      .then(() => self.skipWaiting())
-  );
+// service-worker.js
+self.addEventListener('install', event => {
+  console.log('Service Worker: Installed');
+  self.skipWaiting(); // Activate worker immediately
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keyList =>
-      Promise.all(
-        keyList.map(key => {
-          if (key !== cacheName) {
-            return caches.delete(key);
-          }
-        })
-      )
-    ).then(() => self.clients.claim())
-  );
+self.addEventListener('activate', event => {
+  console.log('Service Worker: Activated');
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
-  );
+self.addEventListener('fetch', event => {
+  // Optional: you can cache or proxy here
 });
