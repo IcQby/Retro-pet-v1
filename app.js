bc22bc8c81728e0a1885450fbe9cfcc232ff8919
IcQby <co.steven@gmail.com>
2025-06-28 21:22:22 +0200

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ window.addEventListener('resize', () => {
 
 // Constants for pet size
 const width = 102, height = 102;  // Actual image size
-const groundY = canvas.height - height - 120;
+const groundY = canvas.height - height - 320;
 
 // Pet image
 let petImgLeft = new Image();
@@ -42,11 +42,11 @@ startJump();
 function drawBackground() {
   // Ground (pastel green)
   ctx.fillStyle = '#90EE90';  // Pastel green color for the ground
-  ctx.fillRect(0, canvas.height * 3 / 4, canvas.width, canvas.height / 4);
+  ctx.fillRect(0, canvas.height * 2 / 3, canvas.width, canvas.height / 3);
 
   // Air (light blue)
   ctx.fillStyle = '#ADD8E6';  // Light blue color for the sky
-  ctx.fillRect(0, 0, canvas.width, canvas.height * 3 / 4);
+  ctx.fillRect(0, 0, canvas.width, canvas.height * 2 / 3);
 }
 
 // Animation function
