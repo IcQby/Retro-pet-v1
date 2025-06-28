148307e6bbd0c0188718824378d5b72ef5dcdcad
IcQby <co.steven@gmail.com>
2025-06-28 14:23:23 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,14 +5,32 @@ const filesToCache = [
   '/style.css',
   '/app.js',
   '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png'
+  '/retro_pet_pig_icon_192.png',
+  '/retro_pet_pig_icon_512.png'
 ];
+
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(cacheName).then(cache => cache.addAll(filesToCache))
+    caches.open(cacheName)
+      .then(cache => cache.addAll(filesToCache))
+      .then(() => self.skipWaiting())
   );
 });
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keyList =>
+      Promise.all(
+        keyList.map(key => {
+          if (key !== cacheName) {
+            return caches.delete(key);
+          }
+        })
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(response => response || fetch(e.request))
