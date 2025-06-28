48b6408dae16af06ddbffc4dd491ebb8a5bda1d2
IcQby <co.steven@gmail.com>
2025-06-28 21:53:01 +0200

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ window.addEventListener('resize', () => {
 
 // Constants for pet size
 const width = 102, height = 102;  // Actual image size
-const groundY = canvas.height - (canvas.height * 2 / 9);  // 2/9ths from the bottom
+const groundY = canvas.height - (canvas.height / 4);  // 1/4ths from the bottom
 
 
 // Pet image
